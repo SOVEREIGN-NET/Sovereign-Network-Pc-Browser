294036d0517a8782bc791367671bea2462492545
Seth Ramsay <404.anomaly.404@gmail.com>
2026-07-22 17:20:40 -0700

    fix: add missing exports to SafeAreaShim to resolve react-navigation build error

--- src/native/SafeAreaShim.tsx
+++ src/native/SafeAreaShim.tsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import { View } from 'react-native';
 
 export const SafeAreaProvider = ({ children }: any) => <View style={{ flex: 1 }}>{children}</View>;
 export const SafeAreaView = ({ children, style }: any) => <View style={[{ flex: 1 }, style]}>{children}</View>;
 export const useSafeAreaInsets = () => ({ top: 0, bottom: 0, left: 0, right: 0 });
+
+export const initialWindowMetrics = {
+  frame: { x: 0, y: 0, width: 0, height: 0 },
+  insets: { top: 0, left: 0, right: 0, bottom: 0 },
+};
+
+export const SafeAreaInsetsContext = createContext({
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+});
+
+export default {
+  SafeAreaProvider,
+  SafeAreaView,
+  useSafeAreaInsets,
+  initialWindowMetrics,
+  SafeAreaInsetsContext,
+};
